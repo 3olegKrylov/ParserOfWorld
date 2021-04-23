e2189054c66ff12bb64d738c1e24b6b538701d19
Oleg <79211838196@yandex.ru>
2021-04-23 18:02:24 +0300

    bag fix

--- cmd/main.go
+++ cmd/main.go
@@ -13,30 +13,14 @@ import (
 
 func main() {
 	urlStr := []string{
-		"https://www.tiktok.com/search?q=олег&lang=ru-RU",
-		"https://www.tiktok.com/search?q=лена&lang=ru-RU",
-		"https://www.tiktok.com/search?q=егор&lang=ru-RU",
-		"https://www.tiktok.com/search?q=влад&lang=ru-RU",
-		"https://www.tiktok.com/search?q=вова&lang=ru-RU",
+		"https://www.tiktok.com/search?q=настюля&lang=ru-RU",
 		"https://www.tiktok.com/search?q=настя&lang=ru-RU",
-		"https://www.tiktok.com/search?q=анжела&lang=ru-RU",
-		"https://www.tiktok.com/search?q=женя&lang=ru-RU",
-		"https://www.tiktok.com/search?q=витя&lang=ru-RU",
-		"https://www.tiktok.com/search?q=ксюша&lang=ru-RU",
-		"https://www.tiktok.com/search?q=полина&lang=ru-RU",
-		"https://www.tiktok.com/search?q=лера&lang=ru-RU",
-		"https://www.tiktok.com/search?q=аня&lang=ru-RU",
-		"https://www.tiktok.com/search?q=лиза&lang=ru-RU",
-		"https://www.tiktok.com/search?q=вика&lang=ru-RU",
-		"https://www.tiktok.com/search?q=дина&lang=ru-RU",
-		"https://www.tiktok.com/search?q=юля&lang=ru-RU",
-		"https://www.tiktok.com/search?q=маша&lang=ru-RU",
-		"https://www.tiktok.com/search?q=степан&lang=ru-RU",
-		"https://www.tiktok.com/search?q=евгений&lang=ru-RU",
-		"https://www.tiktok.com/search?q=антон&lang=ru-RU",
-		"https://www.tiktok.com/search?q=марк&lang=ru-RU",
-		"https://www.tiktok.com/search?q=артём&lang=ru-RU",
-		"https://www.tiktok.com/search?q=бизнес&lang=ru-RU",
+		"https://www.tiktok.com/search?q=анастасия&lang=ru-RU",
+		"https://www.tiktok.com/search?q=настюня&lang=ru-RU",
+		"https://www.tiktok.com/search?q=нутик&lang=ru-RU",
+		"https://www.tiktok.com/search?q=анастэйша&lang=ru-RU",
+		"https://www.tiktok.com/search?q=бизнесменша&lang=ru-RU",
+
 	}
 
 	//подключение к clickhouse
@@ -51,7 +35,6 @@ func main() {
 		//инициализация карты пользователей
 		userMap := db.InitUsers(dbConnect)
 		countOfUsers = int32(len(userMap))
-		fmt.Println("userMap: ", userMap)
 		text := parsing.ParseFindList(urlStr[i])
 
 		lines := strings.Split(text, "\n\n")
@@ -62,7 +45,6 @@ func main() {
 			if strings.HasSuffix(value, "Подписчики") {
 				_, ok := userMap[strings.TrimSpace(lines[num-1])]
 				if !ok {
-					fmt.Println("newUser: ", lines[num-1])
 					newUsers = append(newUsers, lines[num-1])
 				}
 			}
