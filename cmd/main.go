e68ab8685b0f8ffe5149d39b0b2031249de49da9
Oleg <79211838196@yandex.ru>
2021-04-25 12:58:26 +0300

    make tsv

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	internal "github.com/testSpace/internal/data/send"
-	"github.com/testSpace/internal/db"
 	"github.com/testSpace/internal/parsing"
+	"log"
+	"os"
 	"strings"
 	"time"
 
@@ -43,20 +45,36 @@ func main() {
 		"https://www.tiktok.com/search?q=насосала&lang=ru-RU",
 		"https://www.tiktok.com/search?q=таня&lang=ru-RU",
 		"https://www.tiktok.com/search?q=танюша&lang=ru-RU",
-
 	}
 
 	//подключение к clickhouse
-	dbConnect := db.DBconnect()
-	db.DBinit(dbConnect)
-	defer dbConnect.Close()
+	//dbConnect := db.DBconnect()
+	//db.DBinit(dbConnect)
+	//defer dbConnect.Close()
+	var userMap map[string]int32
+	if _, err := os.Stat("users.tsv"); os.IsNotExist(err) {
+		// path/to/whatever does not exist
+	}else{
+		userMap = UserMapInit("users.tsv")
+	}
+
+	file, err := os.OpenFile("users.tsv", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+
+	str := fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "Id", "LinkAccount", "Title", "SubTitle", "Comment", "Mail", "Telegram", "Instagram", "Links", "LanguageAccount", "Phone", "Following", "Followers", "Likes", "LastPostShowTotal", "AverageShows", "MedianShows", "TotalPosts", "LastActionTime", "ParsingTime")
+	file.Write([]byte(str))
 
 	start := time.Now()
 	countOfUsers := int32(0)
+
+	fmt.Println(userMap)
+	fmt.Println(len(userMap))
 	//парсинг аккаунтов
 	for i := 0; i < len(urlStr); i++ {
 		//инициализация карты пользователей
-		userMap := db.InitUsers(dbConnect)
+
 		countOfUsers = int32(len(userMap))
 		text := parsing.ParseFindList(urlStr[i])
 
@@ -73,7 +91,8 @@ func main() {
 			}
 		}
 		if len(newUsers) > 0 {
-			internal.SendData(newUsers, dbConnect, &countOfUsers)
+			//internal.SendData(newUsers, dbConnect, &countOfUsers)
+			internal.SendDataTsv(newUsers, file, &countOfUsers)
 			countOfUsers = countOfUsers + int32(len(newUsers))
 			fmt.Println("Время работы: ", time.Since(start))
 		}
@@ -82,3 +101,46 @@ func main() {
 	fmt.Println("Время: ", time.Since(start), " \nКол-во юзеров: ", countOfUsers)
 
 }
+
+func UserMapInit(fileName string) map[string]int32{
+
+	newMapName := make(map[string]int32)
+
+	tsv, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	scn := bufio.NewScanner(tsv)
+
+	var lines []string
+
+	for scn.Scan() {
+		line := scn.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scn.Err(); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	for num, line := range lines {
+		fmt.Println(line)
+		record := strings.Split(line, "\t")
+		if num>=2 {
+			if len(record) > 3 {
+				_, ok := newMapName[record[2]]
+				if ok {
+				} else {
+					newMapName[record[2]] = int32(0)
+				}
+			}
+		}
+
+	}
+
+	return newMapName
+}
+
